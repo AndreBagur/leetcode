ac2cdf0788b20facf2cde35bc5d1cf27a0dcfb87
Andre Bagur <andre.bagur@gmail.com>
2023-11-29 11:07:34 -0600

    minor formatting changes

--- bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.test.js
+++ bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.test.js
@@ -3,8 +3,8 @@ const maxProfit = require('./bestTimeToBuyAndSellStock.js');
 describe('maxProfit', () => {
   test('returns 5 for [7,1,5,3,6,4]', () => {
     expect(maxProfit([7,1,5,3,6,4])).toBe(5);
-  })
+  });
   test('returns 0 for [7,6,4,3,1]', () => {
     expect(maxProfit([7,6,4,3,1])).toBe(0);
-  })
+  });
 });
