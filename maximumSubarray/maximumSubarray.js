a38033231be1e66a503fb005474a1a5d21e98248
Andre Bagur <andre.bagur@gmail.com>
2023-12-02 10:03:32 -0600

    complete solution for maximumSubarray

--- maximumSubarray/maximumSubarray.js
+++ maximumSubarray/maximumSubarray.js
@@ -36,7 +36,15 @@ Follow up: If you have figured out the O(n) solution, try coding another solutio
  * @return {number}
  */
 var maxSubArray = function(nums) {
+  let currentMax = nums[0];
+  let globalMax = nums[0];
 
+  for (let i = 1; i < nums.length; i++) {
+    currentMax = Math.max(nums[i], currentMax + nums[i]);
+    globalMax = Math.max(currentMax, globalMax);
+  }
+
+  return globalMax;
 };
 
 module.exports = maxSubArray
