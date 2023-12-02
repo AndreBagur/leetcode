8f5a87e6a55216acce11fa9f298b26998465014a
Andre Bagur <andre.bagur@gmail.com>
2023-12-02 10:04:16 -0600

    corrects tests for maximumSubarray

--- maximumSubarray/maximumSubarray.test.js
+++ maximumSubarray/maximumSubarray.test.js
@@ -2,12 +2,12 @@ const maxSubArray = require('./maximumSubarray.js')
 
 describe('maxSubArray', () => {
   it('returns 6 for [-2,1,-3,4,-1,2,1,-5,4]', ()=> {
-    expect(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])).toEqual(6);
+    expect(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])).toBe(6);
   });
   it('returns 1 for [1]', ()=> {
-    expect(maxSubArray([1])).toEqual(1);
+    expect(maxSubArray([1])).toBe(1);
   });
   it('returns 23 for [5,4,-1,7,8]', ()=> {
-    expect(maxSubArray([5,4,-1,7,8])).toEqual(23);
+    expect(maxSubArray([5,4,-1,7,8])).toBe(23);
   });
 });
