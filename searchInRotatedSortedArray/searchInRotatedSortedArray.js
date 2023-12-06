68c0288638689430b0858947f9cf4483098cb625
Andre Bagur <andre.bagur@gmail.com>
2023-12-06 09:13:02 -0600

    complete solution for searchInRotatedSortedArray

--- searchInRotatedSortedArray/searchInRotatedSortedArray.js
+++ searchInRotatedSortedArray/searchInRotatedSortedArray.js
@@ -38,7 +38,29 @@ nums is an ascending array that is possibly rotated.
  * @return {number}
  */
 var search = function(nums, target) {
+  var left = 0;
+  var right = nums.length - 1;
 
+  while (left <= right) {
+      var mid = Math.floor((right + left) / 2);
+
+      if (target === nums[mid]) return mid;
+
+      if (nums[left] <= nums[mid]) {
+          if (nums[left] <= target && target <= nums[mid]) {
+              right = mid - 1;
+          }  else {
+              left = mid + 1;
+          }
+      } else {
+          if (nums[mid] <= target && target <= nums[right]) {
+              left = mid + 1;
+          } else {
+              right = mid - 1;
+          }
+      }
+  }
+  return -1;
 };
 
 module.exports = search
--- searchInRotatedSortedArray/searchInRotatedSortedArray.test.js
+++ searchInRotatedSortedArray/searchInRotatedSortedArray.test.js
@@ -1,13 +1,13 @@
 const search = require('./searchInRotatedSortedArray.js')
 
 describe('search', () => {
-  it('returns 4 for [4,5,6,7,0,1,2]', () => {
-    expect(search([4,5,6,7,0,1,2])).toBe(4);
+  it('returns the index 4 for [4,5,6,7,0,1,2] and target 0', () => {
+    expect(search([4,5,6,7,0,1,2], 0)).toBe(4);
   });
-  it('returns -1 for [4,5,6,7,0,1,2]', () => {
-    expect(search([4,5,6,7,0,1,2])).toBe(-1);
+  it('returns -1 for [4,5,6,7,0,1,2] and target 3', () => {
+    expect(search([4,5,6,7,0,1,2], 3)).toBe(-1);
   });
-  it('returns -1 for [1]', () => {
-    expect(search([1])).toBe(-1);
+  it('returns -1 for [1] and target 0', () => {
+    expect(search([1], 0)).toBe(-1);
   });
 });
