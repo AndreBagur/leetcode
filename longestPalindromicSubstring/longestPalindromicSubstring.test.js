ee12ffe54ba282711ba11bd47027d61752d800f1
Andre Bagur <andre.bagur@gmail.com>
2023-12-12 11:32:07 -0600

    adds tests for longestPalindromicSubstring

---
+++ longestPalindromicSubstring/longestPalindromicSubstring.test.js
@@ -0,0 +1,10 @@
+const longestPalindrome = require('./longestPalindromicSubstring.js')
+
+describe('longestPalindrome', () => {
+  it('returns "bab" for "babad"', () => {
+    expect(longestPalindrome("babad").toBe("bab"));
+  });
+  it('returns "bb" for "cbbd"', () => {
+    expect(longestPalindrome("cbbd")).toBe("bb");
+  })
+});
