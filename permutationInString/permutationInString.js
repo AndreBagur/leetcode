67285e7c9adabd259ac5cd2b97fccc419bbee330
Andre Bagur <andre.bagur@gmail.com>
2023-12-19 15:43:33 -0600

    complete solution for permutationInString

--- permutationInString/permutationInString.js
+++ permutationInString/permutationInString.js
@@ -28,7 +28,26 @@ s1 and s2 consist of lowercase English letters.
  * @return {boolean}
  */
 const checkInclusion = (s1, s2) => {
-
+    if (s1.length > s2.length) return false;
+    let neededChar = {};
+    for (let i = 0; i < s1.length; i++) {
+        neededChar[s1[i]] = (neededChar[s1[i]] || 0) + 1;
+    };
+    let left = 0;
+    let right = 0;
+    let requiredLength = s1.length;
+    while (right < s2.length) {
+        if (neededChar[s2[right]] > 0) requiredLength--;
+        neededChar[s2[right]]--;
+        right++;
+        if (requiredLength === 0) return true;
+        if (right - left === s1.length) {
+            if (neededChar[s2[left]] >= 0) requiredLength++;
+            neededChar[s2[left]]++;
+            left++;
+        }
+    }
+    return false;
 };
 
 module.exports = checkInclusion
