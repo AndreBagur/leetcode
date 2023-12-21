2711067487ee4a9778950e01dda6c5f556863655
Andre Bagur <andre.bagur@gmail.com>
2023-12-21 14:25:52 -0600

    adds tests for binarySearch

---
+++ binarySearch/binarySearch.test.js
@@ -0,0 +1,10 @@
+const search = require('./binarySearch.js');
+
+describe('search', () => {
+  test('returns 4 for [-1,0,3,5,9,12] and 9', () => {
+    expect(search([-1,0,3,5,9,12], 9)).toBe(4);
+  });
+  test('returns -1 for [-1,0,3,5,9,12]', () => {
+    expect(search([-1,0,3,5,9,12]), 2).toBe(-1);
+  });
+});
