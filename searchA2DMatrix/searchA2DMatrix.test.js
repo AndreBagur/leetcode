519f554d899ce5ddb666b4e6429273cdb2503222
Andre Bagur <andre.bagur@gmail.com>
2023-12-23 22:04:56 -0600

    adds tests for searchA2DMatrix

---
+++ searchA2DMatrix/searchA2DMatrix.test.js
@@ -0,0 +1,10 @@
+const searchMatrix = require('./searchA2DMatrix.js');
+
+describe('searchMatrix', () => {
+  test('returns true for [[1,3,5,7],[10,11,16,20],[23,30,34,60]] and 3', () => {
+    expect(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)).toBe(true);
+  });
+  test('returns false for [[1,3,5,7],[10,11,16,20],[23,30,34,60]] and 13', () => {
+    expect(search([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13).toBe(false);
+  });
+});
