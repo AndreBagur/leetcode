97899be3cf153ea147371e75f58ce73d0019ff77
Andre Bagur <andre.bagur@gmail.com>
2023-12-25 10:40:36 -0600

    corrects tests for binarySearch

--- binarySearch/binarySearch.test.js
+++ binarySearch/binarySearch.test.js
@@ -5,6 +5,6 @@ describe('search', () => {
     expect(search([-1,0,3,5,9,12], 9)).toBe(4);
   });
   test('returns -1 for [-1,0,3,5,9,12] and 2', () => {
-    expect(search([-1,0,3,5,9,12]), 2).toBe(-1);
+    expect(search([-1,0,3,5,9,12], 2)).toBe(-1);
   });
 });
