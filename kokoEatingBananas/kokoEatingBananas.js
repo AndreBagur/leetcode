c55bd37237db2b0e75d2475ac5ef7fb9c7b68f05
Andre Bagur <andre.bagur@gmail.com>
2023-12-28 20:57:33 -0600

    complete solution for kokoEatingBananas

--- kokoEatingBananas/kokoEatingBananas.js
+++ kokoEatingBananas/kokoEatingBananas.js
@@ -36,7 +36,23 @@ piles.length <= h <= 109
  * @return {number}
  */
 const minEatingSpeed = (piles, h) => {
+  let min = 1;
+  let max = Math.max(...piles);
+  let best = max;
 
+  const time = speed => piles.reduce((sum, pile) => sum + Math.ceil(pile/speed), 0);
+
+  while (min <= max) {
+    const mid = Math.floor((min + max) / 2);
+
+    if (time(mid) <= h) {
+      best = mid;
+      max = mid - 1;
+    } else
+      min = mid + 1;
+  }
+
+  return best;
 };
 
 module.exports = minEatingSpeed
