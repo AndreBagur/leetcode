d679a8967a47f2560b7b73bad910e68d9c723ee4
Andre Bagur <andre.bagur@gmail.com>
2023-12-29 09:57:40 -0600

    corrects tests for searchA2DMatrix

--- searchA2DMatrix/searchA2DMatrix.test.js
+++ searchA2DMatrix/searchA2DMatrix.test.js
@@ -5,6 +5,6 @@ describe('searchMatrix', () => {
     expect(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)).toBe(true);
   });
   test('returns false for [[1,3,5,7],[10,11,16,20],[23,30,34,60]] and 13', () => {
-    expect(search([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13).toBe(false);
+    expect(search([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13)).toBe(false);
   });
 });
