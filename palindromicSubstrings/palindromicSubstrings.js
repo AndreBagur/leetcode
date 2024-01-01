053f31384bd064a206006c4c5a7fa71f0461a272
Andre Bagur <andre.bagur@gmail.com>
2024-01-01 09:52:53 -0600

    complete solution for palindromicSubstrings

--- palindromicSubstrings/palindromicSubstrings.js
+++ palindromicSubstrings/palindromicSubstrings.js
@@ -30,7 +30,20 @@ s consists of lowercase English letters.
  * @return {number}
  */
 const countSubstrings = (s) => {
+  const expand = (l, r) => {
+    while (l >= 0 && r < s.length && s[l]===s[r] ) {
+        count++;
+        l--;
+        r++;
+    }
+  }
 
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    expand(i, i);
+    expand(i, i + 1);
+  }
+  return count;
 };
 
 module.exports = countSubstrings
