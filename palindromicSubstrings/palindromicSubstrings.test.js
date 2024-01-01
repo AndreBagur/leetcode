06e35c1619342711e9efb4d10fbbbd5f57a07e33
Andre Bagur <andre.bagur@gmail.com>
2024-01-01 09:53:35 -0600

    corrects tests for palindromicSubstring

--- palindromicSubstrings/palindromicSubstrings.test.js
+++ palindromicSubstrings/palindromicSubstrings.test.js
@@ -2,9 +2,9 @@ const countSubstrings = require('./palindromicSubstrings.js')
 
 describe('countSubstrings', () => {
   it('returns 3 for "abc"', ()=> {
-    expect(countSubstrings("abc")).toBe(2);
+    expect(countSubstrings("abc")).toBe(3);
   });
   it('returns 6 for "aaa"', ()=> {
-    expect(maxSubArray("aaa")).toBe(6);
+    expect(countSubstrings("aaa")).toBe(6);
   });
 });
