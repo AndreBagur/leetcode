9a5490c967db70fae80948864083b70e03a882d8
Andre Bagur <andre.bagur@gmail.com>
2024-02-27 09:42:49 -0600

    complete solution for generateParentheses

--- generateParentheses/generateParentheses.js
+++ generateParentheses/generateParentheses.js
@@ -24,7 +24,20 @@ Constraints:
  */
 
 const generateParenthesis = (n) => {
+  const res = [];
 
+  const backtrack = (l, r, s) => {
+    if (s.length === 2 * n) {
+      res.push(s);
+      return;
+    }
+
+    if (l < n) backtrack(l + 1, r, s + '(');
+    if (r < l) backtrack(l, r + 1, s + ')');
+  };
+
+  backtrack(0, 0, '');
+  return res;
 };
 
 module.exports = generateParenthesis
