57d434cafab96dd1ad41c9204fef9b892e521dc8
Andre Bagur <andre.bagur@gmail.com>
2024-03-06 09:37:38 -0600

    complete solution for reorderList

--- reorderList/reorderList.js
+++ reorderList/reorderList.js
@@ -38,7 +38,33 @@ The number of nodes in the list is in the range [1, 5 * 104].
  * @return {void} Do not return anything, modify head in-place instead.
  */
 const reorderList = (head) => {
+  let slow = head;
+  let fast = head;
+  while (fast.next && fast.next.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+  }
 
+  let prev = null;
+  let cur = slow.next;
+  while (cur) {
+      let temp = cur.next;
+      cur.next = prev;
+      prev = cur;
+      cur = temp;
+  }
+
+  slow.next = null;
+
+  let h1 = head;
+  let h2 = prev;
+
+  while (h2) {
+      let temp = h1.next;
+      h1.next = h2;
+      h1 = h2;
+      h2 = temp;
+  }
 };
 
 module.exports = reorderList
