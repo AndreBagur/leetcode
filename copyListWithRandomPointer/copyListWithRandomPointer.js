721289944a9c7fe780ef9710b2ce4e29a4c37be4
Andre Bagur <andre.bagur@gmail.com>
2024-03-09 10:46:15 -0600

    complete solution for copyListWithRandomPointer

--- copyListWithRandomPointer/copyListWithRandomPointer.js
+++ copyListWithRandomPointer/copyListWithRandomPointer.js
@@ -55,7 +55,24 @@ Node.random is null or is pointing to some node in the linked list.
  */
 
 const copyRandomList = (head) => {
+  if (!head) return null;
 
+  const oldToNew = new Map();
+
+  let curr = head;
+  while (curr) {
+      oldToNew.set(curr, new Node(curr.val));
+      curr = curr.next;
+  }
+
+  curr = head;
+  while (curr) {
+      oldToNew.get(curr).next = oldToNew.get(curr.next) || null;
+      oldToNew.get(curr).random = oldToNew.get(curr.random) || null;
+      curr = curr.next;
+  }
+
+  return oldToNew.get(head);
 };
 
 module.exports = copyRandomList
