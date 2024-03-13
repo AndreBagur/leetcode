66a43e429f03eaa62bace122a59472712e134b55
Andre Bagur <andre.bagur@gmail.com>
2024-03-13 09:52:58 -0500

    complete solution for subtreeOfAnotherTree

--- subtreeOfAnotherTree/subtreeOfAnotherTree.js
+++ subtreeOfAnotherTree/subtreeOfAnotherTree.js
@@ -41,7 +41,17 @@ The number of nodes in the subRoot tree is in the range [1, 1000].
  */
 
 const isSubtree = (root, subRoot) => {
-
+  const areEqual = (node1, node2) => {
+    if (!node1 || !node2) return !node1 && !node2;
+    if (node1.val !== node2.val) return false;
+    return areEqual(node1.left, node2.left) && areEqual(node1.right, node2.right);
+  }
+  const dfs = (node) => {
+    if (!node) return false;
+    if (areEqual(node, subRoot)) return true;
+    return dfs(node.left) || dfs(node.right);
+  }
+  return dfs(root);
 };
 
 module.exports = isSubtree
