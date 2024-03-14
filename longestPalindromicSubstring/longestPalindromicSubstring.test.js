1eb9dd3cc643028801e48437caafd5f099bfd288
Andre Bagur <andre.bagur@gmail.com>
2024-03-14 14:08:24 -0500

    updates tests for longestPalindromicSubstring

--- longestPalindromicSubstring/longestPalindromicSubstring.test.js
+++ longestPalindromicSubstring/longestPalindromicSubstring.test.js
@@ -2,7 +2,7 @@ const longestPalindrome = require('./longestPalindromicSubstring.js')
 
 describe('longestPalindrome', () => {
   it('returns "bab" for "babad"', () => {
-    expect(longestPalindrome("babad").toBe("bab"));
+    expect(longestPalindrome("babad")).toBe("bab");
   });
   it('returns "bb" for "cbbd"', () => {
     expect(longestPalindrome("cbbd")).toBe("bb");
