29eacc8d15d7d25cc58f759c38ff5e67f91348c1
Andre Bagur <andre.bagur@gmail.com>
2024-03-16 10:21:24 -0500

    complete solution for removeNthNodeFromEndOfList

--- removeNthNodeFromEndOfList/removeNthNodeFromEndOfList.js
+++ removeNthNodeFromEndOfList/removeNthNodeFromEndOfList.js
@@ -44,7 +44,23 @@ Follow up: Could you do this in one pass?
  */
 
 const removeNthFromEnd = (head, n) => {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy;
+  let second = dummy;
 
+  for (let i = 0; i <=n; i++) {
+      first = first.next;
+  }
+
+  while (first !== null) {
+      first = first.next;
+      second = second.next;
+  }
+
+  second.next = second.next.next;
+
+  return dummy.next;
 };
 
 module.exports = removeNthFromEnd
