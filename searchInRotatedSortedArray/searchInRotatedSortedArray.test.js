c9afc9edaf09fdfefac07bb5bc08b1dacc0558eb
Andre Bagur <andre.bagur@gmail.com>
2024-04-06 20:19:15 -0500

    adds more tests for searchInRotatedSortedArray

--- searchInRotatedSortedArray/searchInRotatedSortedArray.test.js
+++ searchInRotatedSortedArray/searchInRotatedSortedArray.test.js
@@ -1,13 +1,51 @@
 const search = require('./searchInRotatedSortedArray.js')
 
 describe('search', () => {
-  it('returns the index 4 for [4,5,6,7,0,1,2] and target 0', () => {
-    expect(search([4,5,6,7,0,1,2], 0)).toBe(4);
+  test('Example 1', () => {
+    const nums = [4, 5, 6, 7, 0, 1, 2];
+    const target = 0;
+    expect(search(nums, target)).toBe(4);
   });
-  it('returns -1 for [4,5,6,7,0,1,2] and target 3', () => {
-    expect(search([4,5,6,7,0,1,2], 3)).toBe(-1);
+
+  test('Example 2', () => {
+    const nums = [4, 5, 6, 7, 0, 1, 2];
+    const target = 3;
+    expect(search(nums, target)).toBe(-1);
   });
-  it('returns -1 for [1] and target 0', () => {
-    expect(search([1], 0)).toBe(-1);
+
+  test('Example 3', () => {
+    const nums = [1];
+    const target = 0;
+    expect(search(nums, target)).toBe(-1);
+  });
+
+  test('Array with multiple elements, target at the beginning', () => {
+    const nums = [4, 5, 6, 7, 0, 1, 2];
+    const target = 4;
+    expect(search(nums, target)).toBe(0);
+  });
+
+  test('Array with multiple elements, target at the end', () => {
+    const nums = [4, 5, 6, 7, 0, 1, 2];
+    const target = 2;
+    expect(search(nums, target)).toBe(6);
+  });
+
+  test('Array with multiple elements, target not in array', () => {
+    const nums = [4, 5, 6, 7, 0, 1, 2];
+    const target = 8;
+    expect(search(nums, target)).toBe(-1);
+  });
+
+  test('Array with multiple elements, single element, target present', () => {
+    const nums = [1];
+    const target = 1;
+    expect(search(nums, target)).toBe(0);
+  });
+
+  test('Array with multiple elements, single element, target not present', () => {
+    const nums = [1];
+    const target = 2;
+    expect(search(nums, target)).toBe(-1);
   });
 });
